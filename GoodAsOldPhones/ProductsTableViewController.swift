e32bd060df6b6c7c6a014f6e44fe65f9033b0200
Dean Macaulay <dean@magpies.net>
2016-01-14 15:37:07 +1100

    Refactor and added dynamic data to table.

--- GoodAsOldPhones/ProductsTableViewController.swift
+++ GoodAsOldPhones/ProductsTableViewController.swift
@@ -9,22 +9,48 @@
 import UIKit
 
 class ProductsTableViewController: UITableViewController {
+    
+    var productNames: [String]?
+    
+    override func viewDidLoad() {
+        productNames = ["1907 Wall Set","1921 Dial Phone","1937 Desk Set","1984 Motorola Portable"]
+    }
 
     override func tableView(tableView: UITableView, numberOfRowsInSection section: Int)-> Int {
-        return 5
+        if let pNames = productNames {
+            return pNames.count
+        }
+        return 0
     }
     
     override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCellWithIdentifier("ProductCell",forIndexPath: indexPath)
-        cell.textLabel?.text = "Hello World"
-        cell.imageView?.image = UIImage(named: "image-cell1")
+        
+        let productName = productNames?[indexPath.row]
+        
+        if let pName = productName {
+            cell.textLabel?.text = pName
+            cell.imageView?.image = UIImage(named: "image-cell1")
+        }
+        
         return cell
+
     }
 
     override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
         if segue.identifier == "ShowProduct"{
             let productVC = segue.destinationViewController as? ProductViewController
-            productVC?.productName = "Really Old Phone"
+
+            
+            guard let cell = sender as? UITableViewCell,
+                let indexPath = tableView.indexPathForCell(cell) else{
+                    return
+            }
+            
+            
+            productVC?.productName = productNames?[indexPath.row]
+                
+            
         }
     }
 }
