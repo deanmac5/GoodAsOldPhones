198522e4722c7e5b04448915d60181a54196a8aa
Dean Macaulay <dean@magpies.net>
2016-01-14 15:54:21 +1100

    Custom product class

---
+++ GoodAsOldPhones/Product.swift
@@ -0,0 +1,16 @@
+//
+//  Product.swift
+//  GoodAsOldPhones
+//
+//  Created by Dean on 14/01/2016.
+//  Copyright © 2016 Macaulay. All rights reserved.
+//
+
+import Foundation
+
+class Product {
+
+    var name: String?
+    var productImage: String?
+    var cellImage: String?
+}
